cea2bed5801b0328a8413e0e22cfd51ca3b89b10
Ben Gramling <bgramling18@gmail.com>
2024-07-09 21:47:52 -0500

    edit schedule to run every 12 hours and 20 days forward

--- statemachine/cloudwatch.tf
+++ statemachine/cloudwatch.tf
@@ -9,7 +9,7 @@ resource "aws_cloudwatch_log_group" "MySFNLogGroup" {
 
 resource "aws_cloudwatch_event_rule" "step_function_event_rule" {
   name                = "trigger-fixture-retriever"
-  schedule_expression = "cron(0 0 * * ? *)"
+  schedule_expression = "cron(0 0/12 * * ? *)"
   description         = "trigger the fixture retriever step function"
 }
 
@@ -19,7 +19,7 @@ resource "aws_cloudwatch_event_target" "step_function_event_target" {
   arn       = aws_sfn_state_machine.sfn_state_machine.arn
   role_arn  = aws_iam_role.StateMachineRole.arn
   input     = jsonencode({
-    days_forward: 14,
+    days_forward: 20,
     books: [
       "CAESARS",
       "DRAFTKINGS",
