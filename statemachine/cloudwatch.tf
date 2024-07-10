8820fa8818b11de4aaed942bc7405348acd6fa54
Ben Gramling <bgramling18@gmail.com>
2024-07-09 21:37:05 -0500

    created event rule

--- statemachine/cloudwatch.tf
+++ statemachine/cloudwatch.tf
@@ -6,3 +6,24 @@ resource "aws_cloudwatch_log_group" "MySFNLogGroup" {
     prevent_destroy = false
   }
 }
+
+resource "aws_cloudwatch_event_rule" "step_function_event_rule" {
+  name                = "trigger-fixture-retriever"
+  schedule_expression = "cron(0 0 * * ? *)"
+  description         = "trigger the fixture retriever step function"
+}
+
+resource "aws_cloudwatch_event_target" "step_function_event_target" {
+  target_id = "trigger-fixture-retriever"
+  rule      = aws_cloudwatch_event_rule.step_function_event_rule.name
+  arn       = aws_sfn_state_machine.sfn_state_machine.arn
+  role_arn  = aws_iam_role.StateMachineRole.arn
+  input     = jsonencode({
+    days_forward: 14,
+    books: [
+      "CAESARS",
+      "DRAFTKINGS",
+      "FANDUEL"
+    ]
+  })
+}
