177a3b4e8d7e988e54e6543c56ecf25ec2c71639
dannyray44 <dannyray44@hotmail.co.uk>
2024-07-19 22:19:42 +0100

    working version

--- functions/eventscraper/src/eventscrapers/betfair.py
+++ functions/eventscraper/src/eventscrapers/betfair.py
@@ -5,6 +5,8 @@ import boto3
 import requests
 import betfairlightweight
 
+from arbhelpers.event import BookEvent
+
 proxy = f"https://Iv08hg:KelqtNSB@89-34-98-2.ip.ipb.cloud:9443"
 proxies = {"http": proxy, "https": proxy}
 session = requests.Session()
@@ -21,14 +23,15 @@ session.cert = ("/tmp/client-2048.crt", "/tmp/client-2048.key")
 session.verify = False
 
 
-supported_sports = {"Soccer": "1"}
+supported_sports = {"soccer": 1}
 
 
 def get_participants(event_string: str):
-    pass
+    return 
 
 
 def retrieve_fixtures(days_forward: int):
+    events_dict = {}
     
     time_range = {
         "from": datetime.now().isoformat(timespec='milliseconds') + 'Z',
@@ -39,16 +42,33 @@ def retrieve_fixtures(days_forward: int):
                                            os.environ['BETFAIR_PASSWORD'],
                                            app_key=os.environ['BETFAIR_APP_KEY'], 
                                            cert_files=("/tmp/client-2048.crt", "/tmp/client-2048.key"),
+                                           session=session,
                                            lightweight=True)
 
     trading.login()
 
-    for sport_id in supported_sports.keys():
-        events_list = trading.betting.list_events(filter=betfairlightweight.filters.market_filter(event_type_ids=sport_id, market_start_time=time_range))
+    for sport_name, sport_id in supported_sports.items():
+        events_dict[sport_name] = trading.betting.list_events(
+            filter= betfairlightweight.filters.market_filter(
+                event_type_ids=[sport_id], 
+                market_start_time= time_range
+            )
+        )
+
+    # print(f"events_dict = {events_dict['soccer']}")
+
+    for sport_name, events_list in events_dict.items():
         for event in events_list:
-            yield event
+            try:
+                home, away = event['event']['name'].split(' v ')
+            except Exception as e:
+                print(e)
+                continue
+            yield BookEvent(home, away, sport_name, event['event']['id'], 'BETFAIR')
 
     trading.logout()
+
+
 
 
 if __name__ == "__main__":
